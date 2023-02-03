a00b8912459ad90df7ca71fd2868cef40748218d
XPoweR29 <xpower-29@wp.pl>
2023-02-03 15:47:29 +0100

    Add map view and position marker

--- src/components/layout/Map/Map.tsx
+++ src/components/layout/Map/Map.tsx
@@ -1,7 +1,24 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
+import '../../../utils/fix-map-icon';
 import './Map.css';
 
 export const Map = () => {
     return <>
-        <div className="map"></div>
+        <div className="map">
+            <MapContainer center={[49.942750, 19.182023]} zoom={20}>
+                <TileLayer
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[49.942750, 19.182023]}>
+                    <Popup>
+                        <h2>Rondo!</h2>
+                        <p>Kiedyś się tu rozpierdalały samochody.</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     </>
 }
