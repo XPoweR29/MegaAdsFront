ce261cafe76552468da072dd6a05a48a1fd22d24
XPoweR29 <xpower-29@wp.pl>
2023-02-09 16:35:09 +0100

    Map and markers are finished

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,17 +1,27 @@
+import { FormEvent, FormEventHandler, useContext, useState } from 'react'
+import { SearchContext } from '../../../contexts/search.context';
 import { Btn } from '../../common/Btn'
 import './Header.css'
 
 export const Header = () => {
+    const {search, setSearch} = useContext(SearchContext);
+    const [inputVal, setInputVal] = useState(search);
+
+    const setSearchFromLoaclState = (e: FormEvent) => {
+        e.preventDefault();
+        setSearch(inputVal);
+    }
+
     return <>
         <header>
         <h1><strong>Mega</strong> Ogłoszenia </h1>
 
         <Btn text='Dodaj ogłoszenie'/>
 
-        <div className="search">
-            <input type="text" />
+        <form className="search" onSubmit={setSearchFromLoaclState}>
+            <input type="text" value={inputVal} onChange={e=>setInputVal(e.target.value)}/>
             <Btn text='Szukaj'/>
-        </div>
+        </form>
     </header>
     </>
 }
--- src/components/layout/Map/Map.tsx
+++ src/components/layout/Map/Map.tsx
@@ -3,8 +3,26 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 import '../../../utils/fix-map-icon';
 import './Map.css';
+import { useContext, useEffect, useState } from 'react';
+import { SearchContext } from '../../../contexts/search.context';
+import {SimpleAdEntity} from 'types';
+import { SingleAd } from './SingleAd';
 
 export const Map = () => {
+    const {search} = useContext(SearchContext)
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
+
+    useEffect(() => {
+        (async() => {
+            
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const data = (await res.json()) as SimpleAdEntity[];
+
+            setAds(data);
+
+        })();
+    }, [search]);
+
     return <>
         <div className="map">
             <MapContainer center={[49.942750, 19.182023]} zoom={20}>
@@ -12,12 +30,16 @@ export const Map = () => {
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                     attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
                 />
-                <Marker position={[49.942750, 19.182023]}>
-                    <Popup>
-                        <h2>Rondo!</h2>
-                        <p>Kiedyś się tu rozpierdalały samochody.</p>
-                    </Popup>
-                </Marker>
+
+                {
+                    ads.map(ad => (
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <SingleAd id={ad.id}/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
             </MapContainer>
         </div>
     </>
